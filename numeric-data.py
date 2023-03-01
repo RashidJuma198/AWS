3063f74ea221b9559960269c2ab860593e7b492e
Rashidjuma198 <rashidjumaoti@gmail.com>
2023-03-01 13:25:01 +0300

    numeric data types in python

---
+++ numeric-data.py
@@ -0,0 +1,43 @@
+print("Python has three numeric  types : int, float and complex")
+
+#Creating a variable called myValue and assigning it a value of 1 
+#introduction to integer data types
+myValue = 1
+
+#Output the value of myValue
+print(myValue)
+
+#getting the data type of myValue
+print(type(myValue))
+
+#concatinating integer with string value use str()
+print(str(myValue) +" is of data type "+ str(type(myValue)))
+
+#introduction to float data types
+print("***********************************************************************")
+myValue = 3.14
+
+print(myValue)
+
+print(type(myValue))
+
+print(str(myValue)+" is of data type "+str(type(myValue)))
+
+print("************************************************************************")
+#introduction to complex number
+myValue = 5j
+
+print(myValue)
+
+print(type(myValue))
+
+print(str(myValue)+ " is of data type "+ str(type(myValue)))
+
+print("************************************************************************")
+#introduction to bool data type
+
+myValue = True
+
+print(myValue)
+
+print(str(myValue)+" is of data type "+str(type(myValue)))
